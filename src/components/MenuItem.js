14b1ceaccdf1e36e02ec7b3b78c13b38c0c267a4
jordivilagut <vilagutjordi@gmail.com>
2020-03-21 19:16:37 +0100

    Style menu items

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -2,8 +2,12 @@ import React from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const MenuItem = props => (
-    <li>
-        <FontAwesomeIcon icon={props.icon}/>
-        <p>{props.title}</p>
+    <li className="menuItem boxShadow">
+        <div className="itemIconFrame">
+            <FontAwesomeIcon className="itemIcon" icon={props.icon}/>
+        </div>
+        <div class="itemTitle">
+            <p>{props.title}</p>
+        </div>
     </li>
 );
--- src/pages/MainMenu.js
+++ src/pages/MainMenu.js
@@ -1,9 +1,10 @@
 import {MenuItem} from "../components/MenuItem";
 import {faChartLine, faChild, faFutbol, faUser} from "@fortawesome/free-solid-svg-icons";
+import '../styles/MenuItem.css';
 import React from "react";
 
 export const MainMenu = () => (
-    <nav>
+    <nav id="mainMenu">
         <ul>
             <MenuItem id="games" title="Games" icon={faFutbol}/>
             <MenuItem id="teams" title="Teams" icon={faChild}/>
