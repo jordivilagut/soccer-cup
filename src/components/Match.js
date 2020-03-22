adab0be7c40df4e8a4990105018b5cca310c598f
jordivilagut <vilagutjordi@gmail.com>
2020-03-22 15:02:49 +0100

    Add and style games

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,12 +1,32 @@
 import React from "react";
 
 export const Match = props => (
-    <li className="menuItem boxShadow">
-        <div className="itemIconFrame">
-            <img src={require("../assets/badges/" + props.icon)} className="itemIcon"/>
+    <li className="matchItem boxShadow">
+        <div className="matchTeamItem">
+            <div className="itemIconFrame">
+                <img src={require("../assets/badges/" + props.icon1)} className="itemIcon"/>
+            </div>
+            <div className="data">
+                <div className="itemTitle">
+                    <p>{props.name1}</p>
+                </div>
+                <div className="scoreFrame">
+                    <p>{props.score1}</p>
+                </div>
+            </div>
         </div>
-        <div className="itemTitle">
-            <p>{props.name}</p>
+        <div className="matchTeamItem">
+            <div className="itemIconFrame">
+                <img src={require("../assets/badges/" + props.icon2)} className="itemIcon"/>
+            </div>
+            <div className="data">
+                <div className="itemTitle">
+                    <p>{props.name2}</p>
+                </div>
+                <div className="scoreFrame">
+                    <p>{props.score2}</p>
+                </div>
+            </div>
         </div>
     </li>
 );
--- src/pages/Games.js
+++ src/pages/Games.js
@@ -1,16 +1,16 @@
 import React from "react";
-import {Team} from "../components/Team";
 import {MenuItem} from "../components/MenuItem";
 import {faFutbol} from "@fortawesome/free-solid-svg-icons";
+import {Match} from "../components/Match";
+import '../styles/Games.css';
 
 export const Games = () => (
-    <div id="teamsMenu">
+    <div id="gamesMenu">
         <ul>
-            <Team name="F.C. Barcelona" icon="fcb.png"/>
-            <Team name="Manchester City" icon="city.png"/>
-            <Team name="Liverpool F.C." icon="lvp.png"/>
-            <Team name="Real Madrid C.F." icon="rmd.png"/>
-            <Team name="Paris Saint Germain" icon="psg.png"/>
+            <Match name1="Barcelona" name2="Man. City" icon1="fcb.png" icon2="city.png" score1="0" score2="0"/>
+            <Match name1="Liverpool" name2="PSG" icon1="lvp.png" icon2="psg.png" score1="0" score2="0"/>
+            <Match name1="Madrid" name2="Barcelona" icon1="rmd.png" icon2="fcb.png" score1="0" score2="0"/>
+            <Match name1="Man. City" name2="Liverpool" icon1="city.png" icon2="lvp.png" score1="0" score2="0"/>
             <MenuItem href="/" title="Main Menu" icon={faFutbol}/>
         </ul>
     </div>
